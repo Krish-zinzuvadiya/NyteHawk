6e42f8835e3da9dff3c3594f88fc63e9e1638732
Krish Zinzuvadiya <168444725+Krish-zinzuvadiya@users.noreply.github.com>
2025-11-17 10:34:09 +0530

    Update Fuel.js

--- src/components/pages/Fuel.js
+++ src/components/pages/Fuel.js
@@ -9,49 +9,49 @@ const fuelStations = [
     name: "HP Petroleum",
     address: "Ahmedabad",
     directionLink: "https://www.google.com/maps/search/Hindustan+Petroleum+Ahmedabad",
-    image: "/images/fuel/hp.png"
+    image: "/images/Fuel/hp.png"
   },
   {
     name: "Jio BP",
     address: "Ahmedabad",
     directionLink: "https://www.google.com/maps/search/Jio+BP+Ahmedabad",
-    image: "/images/fuel/jiobp.png"
+    image: "/images/Fuel/jiobp.png"
   },
   {
     name: "Bharat Petroleum",
     address: "Ahmedabad",
     directionLink: "https://www.google.com/maps/search/Bharat+Petroleum+Ahmedabad",
-    image: "/images/fuel/bharat.png"
+    image: "/images/Fuel/bharat.png"
   },
   {
     name: "Shell",
     address: "Ahmedabad",
     directionLink: "https://www.google.com/maps/search/Shell+Ahmedabad",
-    image: "/images/fuel/shell.jpg"
+    image: "/images/Fuel/shell.jpg"
   },
   {
     name: "Essar",
     address: "Ahmedabad",
     directionLink: "https://www.google.com/maps/search/Essar+Ahmedabad",
-    image: "/images/fuel/essar.jpeg"
+    image: "/images/Fuel/essar.jpeg"
   },
   {
     name: "Gujarat Gas",
     address: "Ahmedabad",
     directionLink: "https://www.google.com/maps/search/Gujarat+Gas+Ahmedabad",
-    image: "/images/fuel/gujaratgas.png"
+    image: "/images/Fuel/gujaratgas.png"
   },
   {
     name: "Adani Gas",
     address: "Ahmedabad",
     directionLink: "https://www.google.com/maps/search/Adani+Gas+Ahmedabad",
-    image: "/images/fuel/adanigas.png"
+    image: "/images/Fuel/adanigas.png"
   },
   {
     name: "Nayara Energy",
     address: "Ahmedabad",
     directionLink: "https://www.google.com/maps/search/Nayara+Energy+Ahmedabad",
-    image: "/images/fuel/nayara.jpg"
+    image: "/images/Fuel/nayara.jpg"
   }
 ];
 
